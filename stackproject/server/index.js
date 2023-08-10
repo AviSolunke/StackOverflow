b55ff735a907ad31988433d410fbd6dc4e025530
AviSolunke <125122645+AviSolunke@users.noreply.github.com>
2023-08-10 16:46:18 +0530

    Add files via upload

---
+++ stackproject/server/index.js
@@ -0,0 +1,59 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import userRoutes from "./routes/users.js";
+import questionRoutes from "./routes/Questions.js";
+import answerRoutes from "./routes/Answers.js";
+import dotenv from "dotenv";
+import chatbotRoutes from "./routes/Chatbot.js";
+import otpRoutes from "./routes/Otp.js";
+import morgan from "morgan";
+
+const app = express();
+dotenv.config();
+app.use(express.json({ limit: "30mb", extended: true }));
+app.use(express.urlencoded({ limit: "30mb", extended: true }));
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      const allowedOrigins = [
+        "http://localhost:3000",
+        "https://frolicking-sundae-58e3d9.netlify.app",
+        "https://64ca2cb1716e88051c535ff1--vocal-dango-e6e8fa.netlify.app/"
+      ];
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
+
+
+
+app.use(morgan("dev"));
+
+app.get("/", (req, res) => {
+  res.send("This is a stack overflow clone API");
+});
+
+app.use("/user", userRoutes);
+app.use("/questions", questionRoutes);
+app.use("/answer", answerRoutes);
+app.use("/chatbot", chatbotRoutes);
+app.use("/otp", otpRoutes);
+
+const PORT = process.env.PORT || 5000;
+
+const DATABASE_URL = process.env.CONNECTION_URL;
+
+mongoose
+  .connect(DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() =>
+    app.listen(PORT, () => {
+      console.log(`server listening on ${PORT}`);
+    })
+  )
+  .catch((err) => console.log(err.message));
